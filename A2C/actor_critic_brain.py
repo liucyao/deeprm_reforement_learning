0adbc82486f0da28dcf1a4b67787b50b5f1c23e1
JunkFei <635227526@qq.com>
2018-02-09 21:31:17 +0800

    Add files via upload

---
+++ A2C/actor_critic_brain.py
@@ -0,0 +1,95 @@
+import numpy as np
+import tensorflow as tf
+
+# Superparameters
+GAMMA = 0.9     # reward discount in TD error
+
+
+class Actor(object):
+    def __init__(self, sess, n_features, n_actions, lr=0.001):
+        self.sess = sess
+
+        self.s = tf.placeholder(tf.float32, [None, n_features], "state")
+        self.a = tf.placeholder(tf.int32, None, "act")
+        self.td_error = tf.placeholder(tf.float32, None, "td_error")  # TD_error
+
+        with tf.variable_scope('Actor'):
+            l1 = tf.layers.dense(
+                inputs=self.s,
+                units=10,    # number of hidden units
+                activation=tf.nn.relu,
+                kernel_initializer=tf.random_normal_initializer(mean=0.0, stddev=0.3),    # weights
+                bias_initializer=tf.constant_initializer(0.1),  # biases
+                name='l1'
+            )
+
+            self.acts_prob = tf.layers.dense(
+                inputs=l1,
+                units=n_actions,    # output units
+                activation=tf.nn.softmax,   # get action probabilities
+                kernel_initializer=tf.random_normal_initializer(0.0, 0.3),  # weights
+                bias_initializer=tf.constant_initializer(0.1),  # biases
+                name='acts_prob'
+            )
+
+        with tf.variable_scope('exp_v'):
+            log_prob = tf.log(self.acts_prob[0, self.a])
+            self.exp_v = tf.reduce_mean(log_prob * self.td_error)  # advantage (TD_error) guided loss
+
+        with tf.variable_scope('train'):
+            self.train_op = tf.train.AdamOptimizer(lr).minimize(-self.exp_v)  # minimize(-exp_v) = maximize(exp_v)
+
+    def learn(self, s, a, td):
+        # s = s[np.newaxis, :]
+        feed_dict = {self.s: s, self.a: a, self.td_error: td}
+        _, exp_v = self.sess.run([self.train_op, self.exp_v], feed_dict)
+        return exp_v
+
+    def choose_action(self, s):
+        # s = s[np.newaxis, :]
+        probs = self.sess.run(self.acts_prob, feed_dict={self.s: s})   # get probabilities for all actions
+        return np.random.choice(np.arange(probs.shape[1]), p=probs.ravel())   # return a int
+
+
+class Critic(object):
+    def __init__(self, sess, n_features, lr=0.01):
+        self.sess = sess
+
+        self.s = tf.placeholder(tf.float32, [1, n_features], "state")
+        self.v_ = tf.placeholder(tf.float32, [1, 1], "v_next")
+        self.r = tf.placeholder(tf.float32, None, 'r')
+
+        with tf.variable_scope('Critic'):
+            l1 = tf.layers.dense(
+                inputs=self.s,
+                units=10,  # number of hidden units
+                activation=tf.nn.relu,  # None
+                # have to be linear to make sure the convergence of actor.
+                # But linear approximator seems hardly learns the correct Q.
+                kernel_initializer=tf.random_normal_initializer(0., .3),  # weights
+                bias_initializer=tf.constant_initializer(0.1),  # biases
+                name='l1'
+            )
+
+            self.v = tf.layers.dense(
+                inputs=l1,
+                units=1,  # output units
+                activation=None,
+                kernel_initializer=tf.random_normal_initializer(0., .3),  # weights
+                bias_initializer=tf.constant_initializer(0.1),  # biases
+                name='V'
+            )
+
+        with tf.variable_scope('squared_TD_error'):
+            self.td_error = self.r + GAMMA * self.v_ - self.v
+            self.loss = tf.square(self.td_error)    # TD_error = (r+gamma*V_next) - V_eval
+        with tf.variable_scope('train'):
+            self.train_op = tf.train.AdamOptimizer(lr).minimize(self.loss)
+
+    def learn(self, s, r, s_):
+        # s, s_ = s[np.newaxis, :], s_[np.newaxis, :]
+
+        v_ = self.sess.run(self.v, {self.s: s_})
+        td_error, _ = self.sess.run([self.td_error, self.train_op],
+                                    {self.s: s, self.v_: v_, self.r: r})
+        return td_error
---
+++ A2C/pg_re.py
@@ -0,0 +1,325 @@
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import environment
+import job_distribution
+import slow_down_cdf
+import actor_critic_brain
+
+
+def init_accums(pg_learner):  # in rmsprop
+    accums = []
+    params = pg_learner.get_params()
+    for param in params:
+        accum = np.zeros(param.shape, dtype=param.dtype)
+        accums.append(accum)
+    return accums
+
+
+def rmsprop_updates_outside(grads, params, accums, stepsize, rho=0.9, epsilon=1e-9):
+
+    assert len(grads) == len(params)
+    assert len(grads) == len(accums)
+    for dim in range(len(grads)):
+        accums[dim] = rho * accums[dim] + (1 - rho) * grads[dim] ** 2
+        params[dim] += (stepsize * grads[dim] / np.sqrt(accums[dim] + epsilon))
+
+
+def discount(x, gamma):
+    """
+    Given vector x, computes a vector y such that
+    y[i] = x[i] + gamma * x[i+1] + gamma^2 x[i+2] + ...
+    """
+    out = np.zeros(len(x))
+    out[-1] = x[-1]
+    for i in reversed(range(len(x)-1)):
+        out[i] = x[i] + gamma*out[i+1]
+    assert x.ndim >= 1
+    # More efficient version:
+    # scipy.signal.lfilter([1],[1,-gamma],x[::-1], axis=0)[::-1]
+    return out
+
+
+def get_traj(actor, critic, env, episode_max_length):
+    """
+    Run agent-environment loop for one whole episode (trajectory)
+    Return dictionary of results
+    """
+    env.reset()
+    obs = []
+    acts = []
+    rews = []
+    info = []
+
+    ob = env.observe()
+
+    for _ in range(episode_max_length):
+
+        a = actor.choose_action(ob)
+
+        obs.append(ob)  # store the ob at current decision making step
+        acts.append(a)
+
+        ob_, rew, done, info = env.step(a, repeat=True)
+
+        td_error = critic.learn(ob, rew, ob_)
+        actor.learn(ob, a, td_error)
+
+        rews.append(rew)
+
+        if done:
+
+            break
+
+        ob = ob_
+
+    return {'reward': np.array(rews),
+            'ob': np.array(obs),
+            'action': np.array(acts),
+            'info': info,
+            }
+
+
+def concatenate_all_ob(trajs, pa):
+
+    timesteps_total = 0
+    for i in range(len(trajs)):
+        timesteps_total += len(trajs[i]['reward'])
+
+    all_ob = np.zeros(
+        (timesteps_total, pa.network_input_height*pa.network_input_width),
+        dtype=np.float64)
+
+    timesteps = 0
+    for i in range(len(trajs)):
+        for j in range(len(trajs[i]['reward'])):
+            all_ob[timesteps, :] = trajs[i]['ob'][j]
+            timesteps += 1
+
+    return all_ob
+
+
+def process_all_info(trajs):
+    enter_time = []
+    finish_time = []
+    job_len = []
+
+    for traj in trajs:
+        enter_time.append(np.array([traj['info'].record[i].enter_time for i in range(len(traj['info'].record))]))
+        finish_time.append(np.array([traj['info'].record[i].finish_time for i in range(len(traj['info'].record))]))
+        job_len.append(np.array([traj['info'].record[i].len for i in range(len(traj['info'].record))]))
+
+    enter_time = np.concatenate(enter_time)
+    finish_time = np.concatenate(finish_time)
+    job_len = np.concatenate(job_len)
+
+    return enter_time, finish_time, job_len
+
+
+def plot_lr_curve(output_file_prefix, max_rew_lr_curve, mean_rew_lr_curve, slow_down_lr_curve,
+                  ref_discount_rews, ref_slow_down):
+    num_colors = len(ref_discount_rews) + 2
+    cm = plt.get_cmap('gist_rainbow')
+
+    fig = plt.figure(figsize=(12, 5))
+
+    ax = fig.add_subplot(121)
+    ax.set_color_cycle([cm(1. * i / num_colors) for i in range(num_colors)])
+
+    ax.plot(mean_rew_lr_curve, linewidth=2, label='PG mean')
+    for k in ref_discount_rews:
+        ax.plot(np.tile(np.average(ref_discount_rews[k]), len(mean_rew_lr_curve)), linewidth=2, label=k)
+    ax.plot(max_rew_lr_curve, linewidth=2, label='PG max')
+
+    plt.legend(loc=4)
+    plt.xlabel("Iteration", fontsize=20)
+    plt.ylabel("Discounted Total Reward", fontsize=20)
+
+    ax = fig.add_subplot(122)
+    ax.set_color_cycle([cm(1. * i / num_colors) for i in range(num_colors)])
+
+    ax.plot(slow_down_lr_curve, linewidth=2, label='PG mean')
+    for k in ref_discount_rews:
+        ax.plot(np.tile(np.average(np.concatenate(ref_slow_down[k])), len(slow_down_lr_curve)), linewidth=2, label=k)
+
+    plt.legend(loc=1)
+    plt.xlabel("Iteration", fontsize=20)
+    plt.ylabel("Slowdown", fontsize=20)
+
+    plt.savefig(output_file_prefix + "_lr_curve" + ".pdf")
+
+
+def get_traj_worker(actor, critic, env, pa):
+
+    trajs = []
+
+    for i in range(pa.num_seq_per_batch):
+        traj = get_traj(actor, critic, env, pa.episode_max_length)
+        trajs.append(traj)
+
+    # Compute discounted sums of rewards
+    rets = [discount(traj["reward"], pa.discount) for traj in trajs]
+    maxlen = max(len(ret) for ret in rets)
+    padded_rets = [np.concatenate([ret, np.zeros(maxlen - len(ret))]) for ret in rets]
+
+    # Compute time-dependent baseline
+    baseline = np.mean(padded_rets, axis=0)
+
+    # Compute advantage function
+    advs = [ret - baseline[:len(ret)] for ret in rets]
+
+    all_eprews = np.array([discount(traj["reward"], pa.discount)[0] for traj in trajs])  # episode total rewards
+    all_eplens = np.array([len(traj["reward"]) for traj in trajs])  # episode lengths
+
+    # All Job Stat
+    enter_time, finish_time, job_len = process_all_info(trajs)
+    finished_idx = (finish_time >= 0)
+    all_slowdown = (finish_time[finished_idx] - enter_time[finished_idx]) / job_len[finished_idx]
+
+    return all_eprews, all_eplens, all_slowdown
+
+
+def launch(pa, pg_resume=None, render=False, repre='image', end='no_new_job'):
+
+    # ----------------------------
+    print("Preparing for workers...")
+    # ----------------------------
+
+    envs = []
+
+    nw_len_seqs, nw_size_seqs = job_distribution.generate_sequence_work(pa, seed=42)
+
+    for ex in range(pa.num_ex):
+
+        print("-prepare for env-", ex)
+
+        env = environment.Env(pa, nw_len_seqs=nw_len_seqs, nw_size_seqs=nw_size_seqs,
+                              render=False, repre=repre, end=end)
+        env.seq_no = ex
+        envs.append(env)
+
+    print("-prepare for worker-")
+
+    sess = tf.Session()
+
+    actor = actor_critic_brain.Actor(sess, n_features=pa.network_input_height*pa.network_input_width,
+                                     n_actions=pa.network_output_dim, lr=0.001)
+    critic = actor_critic_brain.Critic(sess, n_features=pa.network_input_height*pa.network_input_width,
+                                       lr=0.01)
+    sess.run(tf.global_variables_initializer())
+
+    if pg_resume is not None:
+        pass
+        # rl.load_data(pg_resume)
+
+    # accums = init_accums(pg_learners[pa.batch_size])
+
+    # --------------------------------------
+    print("Preparing for reference data...")
+    # --------------------------------------
+
+    ref_discount_rews, ref_slow_down = slow_down_cdf.launch(pa, pg_resume=None, render=False,
+                                                            plot=False, repre=repre, end=end)
+    mean_rew_lr_curve = []
+    max_rew_lr_curve = []
+    slow_down_lr_curve = []
+
+    # --------------------------------------
+    print("Start training...")
+    # --------------------------------------
+
+    timer_start = time.time()
+
+    for iteration in range(1, pa.num_epochs):
+
+        ex_indices = list(range(pa.num_ex))
+        np.random.shuffle(ex_indices)
+
+        eprewlist = []
+        eplenlist =[]
+        slowdownlist =[]
+
+        ex_counter = 0
+        for ex in range(pa.num_ex):
+
+            ex_idx = ex_indices[ex]
+
+            eprew, eplen, slowdown = get_traj_worker(actor, critic, envs[ex_idx], pa)
+            eprewlist.append(eprew)
+            eplenlist.append(eplen)
+            slowdownlist.append(slowdown)
+
+            ex_counter += 1
+
+            if ex_counter >= pa.batch_size or ex == pa.num_ex - 1:
+
+                print("\n\n")
+
+                ex_counter = 0
+
+        timer_end = time.time()
+
+        print("-----------------")
+        print("Iteration: \t %i" % iteration)
+        # print("NumTrajs: \t %i" % len(eprewlist))
+        print("NumTimesteps: \t %i" % np.sum(eplenlist))
+        # print("MaxRew: \t %s" % np.average([np.max(rew) for rew in eprewlist]))
+        # print("MeanRew: \t %s +- %s" % (np.mean(eprewlist), np.std(eprewlist)))
+        print("MeanSlowdown: \t %s" % np.mean([np.mean(sd) for sd in slowdownlist]))
+        print("MeanLen: \t %s +- %s" % (np.mean(eplenlist), np.std(eplenlist)))
+        print("Elapsed time\t %s" % (timer_end - timer_start), "seconds")
+        print("-----------------")
+
+        timer_start = time.time()
+
+        max_rew_lr_curve.append(np.average([np.max(rew) for rew in eprewlist]))
+        mean_rew_lr_curve.append(np.mean(eprewlist))
+        slow_down_lr_curve.append(np.mean([np.mean(sd) for sd in slowdownlist]))
+
+        if iteration % pa.output_freq == 0:
+
+            # rl.save_data(pa.output_filename + '_' + str(iteration))
+
+            pa.unseen = True
+            # slow_down_cdf.launch(pa, pa.output_filename + '_' + str(iteration) + '.ckpt',
+                                # render=False, plot=True, repre=repre, end=end)
+            pa.unseen = False
+            # test on unseen examples
+
+            plot_lr_curve(pa.output_filename,
+                          max_rew_lr_curve, mean_rew_lr_curve, slow_down_lr_curve,
+                          ref_discount_rews, ref_slow_down)
+
+
+def main():
+
+    import parameters
+
+    pa = parameters.Parameters()
+
+    pa.simu_len = 50  # 1000
+    pa.num_ex = 50  # 100
+    pa.num_nw = 10
+    pa.num_seq_per_batch = 20
+    pa.output_freq = 50
+    pa.batch_size = 10
+
+    # pa.max_nw_size = 5
+    # pa.job_len = 5
+    pa.new_job_rate = 0.3
+
+    pa.episode_max_length = 2000  # 2000
+
+    pa.compute_dependent_parameters()
+
+    pg_resume = None
+    # pg_resume = 'data/tmp_450.pkl'
+
+    render = False
+
+    launch(pa, pg_resume, render, repre='image', end='all_done')
+
+
+if __name__ == '__main__':
+    main()
